990f962eeb8c828a714bdb38610b84c2b5aec328
Cristian Rojas <ca.rojas@globant.com>
2023-09-04 21:04:38 -0500

    Second commit, configuration of the test environment

--- src/main/java/org/saucedemo/Reporter.java
+++ src/main/java/org/saucedemo/Reporter.java
@@ -1,13 +1,13 @@
 package org.saucedemo;
 
-/**
- * Hello world!
- *
- */
-public class Reporter
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.slf4j.LoggerFactory;
+
+public class Reporter {
+   private static org.slf4j.Logger getLogger() { return LoggerFactory.getLogger(Reporter.class); }
+
+   public static void logInfo(String text) { getLogger().info(text); }
+
+   public static void logError(String text) { getLogger().error(text); }
+
+   public static void logWarning(String text) { getLogger().warn(text); }
 }
---
+++ src/test/java/org/saucedemo/Listener.java
@@ -0,0 +1,31 @@
+package org.saucedemo;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import static java.lang.String.format;
+
+public class Listener extends Reporter implements ITestListener {
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        logInfo(format("Test: %s - [FAIL]", result.getName()));
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        logWarning(format("Test: %s - [SKIPPED]", result.getName()));
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        logInfo(format("Test: %s - [Starting...]", result.getName()));
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        logInfo(format("Test: %s - [PASSED]", result.getName()));
+
+    }
+
+}
